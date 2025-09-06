df950ad4ce1755444f3a3f0928b15b47888ca722
Jarodriguo <jarodriguo@eafit.edu.co>
2025-09-06 12:01:45 -0500

    Cambios realizados hasta ahora en el master

--- serv_persistor/src/main/java/com/gridmr/master/model/TaskType.java
+++
@@ -1,7 +0,0 @@
-package com.gridmr.master.model;
-
-// Tipos de tareas que puede ejecutar un worker
-public enum TaskType {
-    MAP,        // Tarea de mapeo
-    REDUCE      // Tarea de reducción
-}
